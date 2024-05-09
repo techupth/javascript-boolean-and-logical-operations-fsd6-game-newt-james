acad1d3920b6ce28f1df472ca2b07d663f972f39
tanayod <tanayod741@gmail.com>
2024-05-09 14:50:54 +0700

    feat:ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,5 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
-
-// Start coding here
+let isOver18 = false;
+let hasCriminalBlacklist = false;
+let isAllow = isOver18 && hasCriminalBlacklist;
 
 console.log(isAllow);
