99a837d69e8bff2e881893c541ac956022a5270c
Thanasak <ma.thanasak@gmail.com>
2024-05-09 14:42:31 +0700

    feat: Do Exam 1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,5 @@
-let isOver18;
-let hasCriminalBlacklist;
-
+let isOver18 = false;
+let hasCriminalBlacklist = false;
 let isAllow;
-
-// Start coding here
-
+isAllow = isOver18 && hasCriminalBlacklist;
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -6,7 +6,16 @@ let isWeekday;
 let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
-
 let hasPromotion;
-
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = true;
+hasAttendedDiscountEvent = false;
+isPlatinum = false;
+hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    hasBoughtProductFromITCategory &&
+    hasAttendedDiscountEvent) ||
+  isPlatinum;
 console.log(hasPromotion);
